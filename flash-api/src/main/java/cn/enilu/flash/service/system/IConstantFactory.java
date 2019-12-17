4fdeedc86647f5906d2ef3c088d99f6d02a9c0c1
niuyu <niuyu107@163.com>
2019-12-17 21:27:04 +0800

    删除字典功能

--- flash-api/src/main/java/cn/enilu/flash/service/system/IConstantFactory.java
+++ flash-api/src/main/java/cn/enilu/flash/service/system/IConstantFactory.java
@@ -1,10 +1,6 @@
 package cn.enilu.flash.service.system;
 
-
 import cn.enilu.flash.bean.entity.system.*;
-import cn.enilu.flash.bean.vo.DictVo;
-
-import java.util.List;
 
 /**
  * 常量生产工厂的接口
@@ -61,46 +57,6 @@ public interface IConstantFactory {
     String getMenuNameByCode(String code);
 
     /**
-     * 根据字典名称获取字典列表
-     * @param dictName
-     * @return
-     */
-    List<DictVo> findByDictName(String dictName);
-    /**
-     * 获取字典名称
-     */
-    String getDictName(Long dictId);
-
-    /**
-     * 根据字典名称和字典中的值获取对应的名称
-     */
-    String getDictsByName(String name, String val);
-
-    /**
-     * 获取性别名称
-     */
-    String getSexName(Integer sex);
-    /**
-     * 获取银行卡类型名称
-     * @param cardType
-     * @return
-     */
-    String getCardTypeName(String cardType);
-
-    /**
-     * 获取个人证件类型
-     * @param cardType
-     * @return
-     */
-    String getIdCardTypeName(String cardType);
-
-    /**
-     * 获取联系人关系
-     * @param relation
-     * @return
-     */
-    String getRelationName(String relation);
-    /**
      * 获取用户登录状态
      */
     String getStatusName(Integer status);
@@ -111,21 +67,9 @@ public interface IConstantFactory {
     String getMenuStatusName(Integer status);
 
     /**
-     * 查询字典
-     */
-    List<Dict> findInDict(Long id);
-
-    /**
      * 获取被缓存的对象(用户删除业务)
      */
     String getCacheObject(String para);
-
-    /**
-     * 获取指定名称下的字典列表
-     * @param pname
-     * @return
-     */
-    List<Dict> getDicts(String pname);
 
     Role getRole(Long id) ;
 
--- flash-api/src/main/java/cn/enilu/flash/service/system/impl/ConstantFactory.java
+++ flash-api/src/main/java/cn/enilu/flash/service/system/impl/ConstantFactory.java
@@ -4,9 +4,7 @@ import cn.enilu.flash.bean.constant.cache.CacheKey;
 import cn.enilu.flash.bean.constant.state.ManagerStatus;
 import cn.enilu.flash.bean.constant.state.MenuStatus;
 import cn.enilu.flash.bean.entity.system.*;
-import cn.enilu.flash.bean.vo.DictVo;
 import cn.enilu.flash.bean.vo.SpringContextHolder;
-import cn.enilu.flash.cache.DictCache;
 import cn.enilu.flash.dao.system.*;
 import cn.enilu.flash.service.system.IConstantFactory;
 import cn.enilu.flash.service.system.LogObjectHolder;
@@ -17,8 +15,6 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 
@@ -34,8 +30,6 @@ import java.util.Optional;
 public class ConstantFactory implements IConstantFactory {
     public static TimeCacheMap<String, String> cache = new TimeCacheMap<String, String>(3600, 2);
     private RoleRepository roleRepository = SpringContextHolder.getBean(RoleRepository.class);
-    private DictCache dictCache = SpringContextHolder.getBean(DictCache.class);
-    private DictRepository dictRepository = SpringContextHolder.getBean(DictRepository.class);
     private UserRepository userRepository = SpringContextHolder.getBean(UserRepository.class);
     private MenuRepository menuRepository = SpringContextHolder.getBean(MenuRepository.class);
 
@@ -49,7 +43,6 @@ public class ConstantFactory implements IConstantFactory {
 
     public void set(String key, String val) {
         cache.put(key, val);
-
     }
 
     /**
@@ -197,82 +190,6 @@ public class ConstantFactory implements IConstantFactory {
         }
     }
 
-    @Override
-    public List<DictVo> findByDictName(String dictName) {
-
-        List<DictVo> list = new ArrayList<DictVo>();
-
-        List<Dict> dicts = dictCache.getDictsByPname(dictName);
-        if (dicts != null) {
-            for (int i = 0; i < dicts.size(); i++) {
-                Dict dict = dicts.get(i);
-                DictVo dictVo = new DictVo(dict.getNum(), dict.getName());
-                list.add(dictVo);
-            }
-        }
-        return list;
-    }
-
-    /**
-     * 获取字典名称
-     */
-    @Override
-    public String getDictName(Long dictId) {
-
-        String val = get(CacheKey.DICT_NAME + dictId);
-        if (StringUtil.isNotEmpty(val)) {
-            return val;
-        }
-        val = dictCache.getDict(dictId);
-        set(CacheKey.DICT_NAME + dictId, val);
-        return val;
-
-    }
-
-    /**
-     * 根据字典名称和字典中的值获取对应的名称
-     */
-    @Override
-    public String getDictsByName(String name, String val) {
-        String result = get(CacheKey.DICT_NAME + name + val);
-        if (StringUtil.isNotEmpty(result)) {
-            return result;
-        }
-        List<Dict> dicts = dictCache.getDictsByPname(name);
-        for (Dict item : dicts) {
-            if (item.getNum() != null && item.getNum().equals(val)) {
-                result = item.getName();
-                set(CacheKey.DICT_NAME + name + val, result);
-                return result;
-            }
-        }
-        return "";
-
-    }
-
-    /**
-     * 获取性别名称
-     */
-    @Override
-    public String getSexName(Integer sex) {
-        return getDictsByName("性别", String.valueOf(sex));
-    }
-
-    @Override
-    public String getCardTypeName(String cardType) {
-        return getDictsByName("银行卡类型", cardType);
-    }
-
-    @Override
-    public String getIdCardTypeName(String cardType) {
-        return getDictsByName("证件类型", cardType);
-    }
-
-    @Override
-    public String getRelationName(String relation) {
-        return getDictsByName("联系人关系", relation);
-    }
-
     /**
      * 获取用户登录状态
      */
@@ -290,25 +207,11 @@ public class ConstantFactory implements IConstantFactory {
     }
 
     /**
-     * 查询字典
-     */
-    @Override
-    public List<Dict> findInDict(Long id) {
-        return dictRepository.findByPid(id);
-
-    }
-
-    /**
      * 获取被缓存的对象(用户删除业务)
      */
     @Override
     public String getCacheObject(String para) {
         return LogObjectHolder.me().get().toString();
-    }
-
-    @Override
-    public List<Dict> getDicts(String pname) {
-        return dictCache.getDictsByPname(pname);
     }
 
     @Override
